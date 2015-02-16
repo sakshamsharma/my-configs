b41e89a0411cbd37728836ecba22a243337e9662
Saksham Sharma <saksham0808@gmail.com>
2015-02-16 12:52:56 +0530

    Vimperator files added.

--- .myscripts/add_rc.sh
+++ .myscripts/add_rc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~
-git add .vimrc .bashrc .conky/* .i3/* .config/touchegg/touchegg.conf README.md .gitignore .fonts/* .myscripts/*
+git add .vimrc .bashrc .conky/* .i3/* .config/touchegg/touchegg.conf README.md .gitignore .fonts/* .myscripts/* .vimperator/* .vimperatorrc
 
