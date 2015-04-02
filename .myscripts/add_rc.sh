add06b8ca37b475e9bdc545dda61cb89851bd9be
Saksham Sharma <saksham0808@gmail.com>
2015-04-02 23:55:28 +0530

    omz2

--- .myscripts/add_rc.sh
+++ .myscripts/add_rc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~
-git add .vimrc .bashrc .conky/* .i3/* .config/touchegg/touchegg.conf README.md .gitignore .fonts/* .myscripts/* .vimperator/* .vimperatorrc .zshrc .zshrc.pre-oh-my-zsh .oh-my-zsh/* start_hotspot.sh
+git add .vimrc .bashrc .conky/* .i3/* .config/touchegg/touchegg.conf README.md .gitignore .fonts/* .myscripts/* .vimperator/* .vimperatorrc .zshrc .zshrc.pre-oh-my-zsh .zshrc-without-omz .oh-my-zsh/* start_hotspot.sh
 
