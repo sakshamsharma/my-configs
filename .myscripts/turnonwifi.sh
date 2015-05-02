c2124829828bfd49e27dd8251babc6ea5d108e7d
Saksham Sharma <saksham0808@gmail.com>
2015-05-03 00:32:14 +0530

    Start hospot now reloads the driver. BUG: if it is already loaded, it would be trouble.

--- .myscripts/turnonwifi.sh
+++ .myscripts/turnonwifi.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Script to turn on my WiFi interfaces on my laptop.
 
-interface=$(rfkill list | grep -E "([L][A][N])" | grep -E "^[0-9]" -o)
+interface=$(rfkill list | grep -E "(LAN)" | grep -E "^[0-9]" -o)
 for x in $interface;
 do
 	rfkill unblock $x
