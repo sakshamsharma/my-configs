113752a4c8909d2e1c85ea768af1802fbec96501
Saksham Sharma <saksham0808@gmail.com>
2015-05-09 15:06:09 +0530

    Complete config folder added to repo (except google-chrome)

--- .myscripts/add_rc.sh
+++ .myscripts/add_rc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~
-git add .vimrc .bashrc .conkyrc .conky/* .conky/.conky-vision-icons/*  .i3/* .config/touchegg/touchegg.conf README.md .gitignore .fonts/* .myscripts/* .vimperatorrc .zshrc .zshrc-without-omz .oh-my-zsh/* start_hotspot.sh .xinitrc .inscripts/*
+git add .vimrc .bashrc .conkyrc .conky/* .conky/.conky-vision-icons/*  .i3/* .config/* README.md .gitignore .fonts/* .myscripts/* .vimperatorrc .zshrc .zshrc-without-omz .oh-my-zsh/* start_hotspot.sh .xinitrc .inscripts/*
 
