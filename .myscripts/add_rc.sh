47fe3b3119a73c780ef84e4f380a2a4bd2a30939
Saksham Sharma <saksham0808@gmail.com>
2015-06-10 20:14:51 +0530

    Removed useless skype config

--- .myscripts/add_rc.sh
+++ .myscripts/add_rc.sh
@@ -5,5 +5,5 @@ git add .vimrc .bashrc .conkyrc .conky/* .conky/.conky-vision-icons/*  			\
 	.zshrc .zshrc-without-omz .oh-my-zsh/* start_hotspot.sh .xinitrc 			\
 	.inscripts/* .tmux.conf .info/* .Xresources									\
 	.config/fish/* .config/gconf/* .config/gtk-2.0/* 							\
-	.config/gtk-3.0/* .config/nautilus/* .config/Skype/*                   		\
+	.config/gtk-3.0/* .config/nautilus/* 				                  		\
 	.config/touchegg/* .config/xfce4/*
