fc70f4cb4e7c0c3961056b5a9138dddcf57aa5ff
Saksham Sharma <saksham0808@gmail.com>
2015-11-29 15:10:54 +0530

    Emacs tweaks for haskell

--- .emacs.d/my-loadpackages.el
+++ .emacs.d/my-loadpackages.el
@@ -66,6 +66,31 @@
 (tabbar-mode 1)
 
 (require 'haskell-mode)
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t)
+	'(haskell-tags-on-save))
+
+(require 'speedbar)
+(speedbar-add-supported-extension ".hs")
+
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+(define-key haskell-mode-map (kbd "C-x C-s") 'haskell-mode-save-buffer)
+
+;; To jump to the location of the top-level identifier at point, run
+(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)
 
 (require 'yasnippet)
 
