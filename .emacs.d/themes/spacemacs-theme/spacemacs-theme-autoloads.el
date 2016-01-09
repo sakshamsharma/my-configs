7c57fc2d8ecaf0b8658074d8b0d15b168cc97c54
Saksham Sharma <saksham0808@gmail.com>
2016-01-09 15:46:36 +0530

    Removed files of vim, emacs, xmonad and i3 shifted to other repos.

--- .emacs.d/themes/spacemacs-theme/spacemacs-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; spacemacs-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (22083
-;;;;;;  10029 892182 719000))
-;;; Generated autoloads from spacemacs-common.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
-;;;;;;  "spacemacs-theme-pkg.el") (22083 10029 982497 997000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; spacemacs-theme-autoloads.el ends here
